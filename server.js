e54be47c8dc9bef54ed31e0e8c0918ea2ca4f219
saurabhsigma <saurabh962003@gmail.com>
2024-03-20 00:28:10 +0530

    path bug fixed

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const app = express();
-app.set('views', path.join(__dirname, 'views'));
+// app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.static('public'))
 app.use(express.urlencoded({extended: true}))
