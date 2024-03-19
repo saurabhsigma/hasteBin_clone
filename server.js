53d7d5decdc02042f3644846d5801e1e38cd88c9
saurabhsigma <saurabh962003@gmail.com>
2024-03-20 00:18:09 +0530

    views fixed

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const app = express();
-app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 app.use(express.static('public'))
 app.use(express.urlencoded({extended: true}))
 
